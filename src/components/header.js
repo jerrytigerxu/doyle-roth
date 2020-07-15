5aa360350c7d7b5ba042bd915400f1f6db4426a1
jerrytigerxu <jere.tiger.xu@gmailc.om>
2020-07-15 16:51:13 -0400

    simplified

--- src/components/header.js
+++ src/components/header.js
@@ -5,15 +5,13 @@ import './style.scss';
 
 import gatsbyLogo from '../images/gatsby-icon.png';
 import bulmaLogo from '../images/bulma-logo.png';
-import Navbar from './navbar';
 
 const Header = ({ siteTitle }) => (
 	<section className="hero gradientBg is-fullheight-with-navbar">
-		<Navbar />
 		<div className="hero-body">
 			<div className="container center">
 				<article className="media">
-					<figure className="is-left">
+					{/*<figure className="is-left">
 						<span className="icon is-large ">
 							<img src={gatsbyLogo} alt="gatsby-logo" />
 						</span>
@@ -22,23 +20,13 @@ const Header = ({ siteTitle }) => (
 						<span className="icon is-large">
 							<img src={bulmaLogo} alt="bulma-logo" />
 						</span>
-					</figure>
+					</figure>*/}
 					<div className="media-content">
 						<div className="content">
 							<h1 className="is-uppercase is-size-1 has-text-white">
-								Hello from Bulma + Gatsby
+								Doyle Roth
 							</h1>
 							<p className="subtitle has-text-white is-size-3">
-								A Bulma CSS + GatsbyJS Starter Kit{' '}
-								<a
-									className="button is-info is-inverted"
-									href="https://github.com/amandeepmittal/gatsby-bulma-quickstart"
-								>
-									<span className="icon">
-										<FaGithub size="fa-2x" />
-									</span>
-									<span>Download</span>
-								</a>
 							</p>
 						</div>
 					</div>
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,14 +5,16 @@ import Helmet from './helmet';
 import Header from './header';
 import Midsection from './midsection';
 import Footer from './footer';
+import Navbar from './navbar';
 
 const Layout = ({ children }) => (
 	<div>
 		<Helmet />
+		<Navbar />
 		<Header />
-		<Midsection />
+		{/*<Midsection />*/}
 		{children}
-		<Footer />
+		{/*<Footer />*/}
 	</div>
 );
 
