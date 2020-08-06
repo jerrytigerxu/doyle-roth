d752e83c038dbb01704f79546eb54721e8d2354d
jerrytigerxu <jere.tiger.xu@gmailc.om>
2020-08-06 10:37:40 -0400

    images for about us

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,30 +2,54 @@ import React from 'react';
 
 import Layout from '../components/layout';
 
+import Img from 'gatsby-image'
+
+import { graphql } from 'gatsby';
+
 const AboutPage = ({ data }) => (
   <Layout>
         <div>
           <section className="section">
             <div className="container">
+              <div className="columns is-multiline">
 
-              <div className="column">
-                <article className="media">
-                  <div className="media-content">
-                    <div className="content">
-                      <h1 className="title is-size-4">About Us</h1>
-                      <p className="subtitle is-size-5">
-                      Doyle & Roth Manufacturing Company (D&R) is a small, women owned well-respected engineering, design and fabrication company established in 1932.  Our main activities are in the area of tubular heat exchangers. We fabricate approximately 300-400 shell and tubeheat exchangers and pressure vessels annually for a wide customer base in varying industries.  We are an engineering intensive organization, our engineers function in Sales, Marketing, General Management and hold Executive level positions.
-                      </p>
-                      <p>[Need to put in photos]</p>
+                <div className="column is-full">
+                  <article className="media">
+                    <div className="media-content">
+                      <div className="content">
+                        <h1 className="title is-size-4">About Us</h1>
+                        <p className="subtitle is-size-5">
+                        Doyle & Roth Manufacturing Company (D&R) is a small, women owned well-respected engineering, design and fabrication company established in 1932.  Our main activities are in the area of tubular heat exchangers. We fabricate approximately 300-400 shell and tubeheat exchangers and pressure vessels annually for a wide customer base in varying industries.  We are an engineering intensive organization, our engineers function in Sales, Marketing, General Management and hold Executive level positions.
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                </article>
+                  </article>
+                </div>
+
+                <div className="column is-one-half">
+                  <article className="media">
+                    <div className="media-content">
+                      <div className="content">
+                        <Img fixed={data.ny.childImageSharp.fixed} alt="New York office"/>
+                        <p className="subtitle is-size-7">Pictured Above: Corporate Headquarters Staff, New York City </p>
+                      </div>
+                    </div>
+                  </article>
+                </div>
+
+                <div className="column is-one-half">
+                  <article className="media">
+                    <div className="media-content">
+                      <div className="content">
+                        <Img fixed={data.penn.childImageSharp.fixed} alt="Pennslyvania office"/>
+                        <p className="subtitle is-size-7">Pictured Above: Fabrication Facility Management & Engineering Staff, Eastern Pennsylvania</p>
+                      </div>
+                    </div>
+                  </article>
+                </div>
+
+
               </div>
-
-
-
-
-
             </div>
           </section>
         </div>
@@ -34,3 +58,26 @@ const AboutPage = ({ data }) => (
 
 
 export default AboutPage
+
+
+export const query = graphql`
+  query {
+		ny: file(absolutePath: {eq: "/home/jeretigerxu/Documents/Hacker Stuff/freelance-projects/doyle-roth/src/images/About Us/730_2017_NY-Website.jpg"}) {
+      childImageSharp {
+        fixed(width: 650) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+
+    penn: file(absolutePath: {eq: "/home/jeretigerxu/Documents/Hacker Stuff/freelance-projects/doyle-roth/src/images/About Us/730_2017_Shop-Website.jpg"}) {
+      childImageSharp {
+        fixed(width:650) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+
+}
+
+`
