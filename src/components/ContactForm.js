3e0f9c684590d9f864baf8adaf2ae6dbb1c0136b
jerrytigerxu <jere.tiger.xu@gmailc.om>
2020-08-13 08:16:55 -0400

    contact form

---
+++ src/components/ContactForm.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+class ContactForm extends React.Component {
+  render () {
+    const formStyle = {
+      textAlign: 'center',
+      lineHeight: '4em',
+    }
+    const buttonsStyle = {
+      margin: '1rem',
+      textAlign: 'center',
+    }
+    return (
+
+        <form style={formStyle} className="form" action={this.props.action} method="post">
+          <div>
+            <label>Full name</label> &nbsp;
+            <input type="text" name="fullname"/>
+          </div>
+          <div>
+            <label>Email</label> &nbsp;
+            <input type="email" name="email"/>
+          </div>
+          <div>
+            <label>Message</label> &nbsp;
+            <textarea name="message" rows="5"></textarea>
+          </div>
+          <ul className="actions" style={buttonsStyle}>
+              <li>
+                <button type="submit"  className="button special">Send</button>
+              </li>
+          </ul>
+        </form>
+    );
+  }
+}
+
+export default ContactForm
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import ContactForm from '../components/ContactForm';
 
 const ContactPage = ({ data }) => {
 
   return (
         <Layout>
+          <ContactForm />
 
         </Layout>
 
