d47edf7ca2cc6bceb21e246db5d6cb2093cd803d
jerrytigerxu <jere.tiger.xu@gmailc.om>
2020-08-21 12:41:10 -0400

    contact

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import './style.scss';
 import Helmet from './helmet';
-import Footer from './footer';
 import Navbar from './navbar';
 
 const Layout = ({ children }) => (
@@ -10,7 +9,6 @@ const Layout = ({ children }) => (
 		<Helmet />
 		<Navbar />
 		{children}
-		<Footer />
 	</div>
 );
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import ContactForm from '../components/ContactForm';
+import Footer from '../components/footer';
 
 const ContactPage = ({ data }) => {
 
   return (
         <Layout>
-          <ContactForm />
-
+          <ContactForm action="https://wwww.flexyform.com/f/d5aadbc542092fef05cd63b90a397b70cae3e56f"/>
+          <Footer />
         </Layout>
 
 
